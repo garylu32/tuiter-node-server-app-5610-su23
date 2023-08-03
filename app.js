672b1170aebde8bb75e7718c3810d94d4de3a3be
Chenhao Lu <garylu32@gmail.com>
2023-08-03 00:18:50 -0700

    RESTful API update

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 import express from 'express';
+import HelloController from "./controllers/hello-controller.js";
+import UserController from "./user/users-controller.js";
+import TuitsController from "./controllers/tuits/tuits-controller.js";
+
 const app = express();
-app.get('/hello', (req, res) => {res.send('Life is Good!')})
-app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
+app.use(express.json());
+TuitsController(app);
+HelloController(app)
+UserController(app)
 app.listen(4000)
